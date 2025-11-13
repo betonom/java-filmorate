15a863dfd2f53fa1da3c4cf6951b8166604cc927
betonom <MrEnot137@yandex.ru>
2025-11-13 19:19:29 +0300

    Добавить логирование для операций получения/добавления/изменения сущностей User и Film

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
@@ -20,22 +22,30 @@ public class FilmController {
 
     @GetMapping
     public Collection<Film> getAll() {
+        log.info("Start handling request with GET method for /films");
+
         return films.values();
     }
 
     @PostMapping
     public Film add(@Valid @RequestBody Film film) {
+        log.info("Start handling request with POST method for /films");
+
         film.setId(getNextId());
+        log.trace("Put film into hashMap films");
         films.put(film.getId(), film);
         return film;
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film newFilm) {
+        log.info("Start handling request with PUT method for /films");
+
         if (newFilm.getId() == null) {
             throw new ConditionsNotMetException("Id должен быть указан", "id");
         }
         if (films.containsKey(newFilm.getId())) {
+            log.trace("Updating old film object's fields");
             Film oldFilm = films.get(newFilm.getId());
             oldFilm.setName(newFilm.getName());
             oldFilm.setDescription(newFilm.getDescription());
@@ -60,14 +70,17 @@ public class FilmController {
                 String errorMessage = error.getDefaultMessage();
                 errors.put(fieldName, errorMessage);
             });
+            log.warn("MethodArgumentNotValidException on handle /films");
         }
         if (ex.getClass().equals(ConditionsNotMetException.class)) {
             ConditionsNotMetException e = (ConditionsNotMetException) ex;
             errors.put(e.getFieldName(), e.getMessage());
+            log.warn("ConditionsNotMetException on handle /films");
         }
         if (ex.getClass().equals(NotFoundException.class)) {
             NotFoundException e = (NotFoundException) ex;
             errors.put(e.getFieldName(), e.getMessage());
+            log.warn("NotFoundException on handle /films");
         }
 
         return errors;
@@ -75,6 +88,7 @@ public class FilmController {
 
 
     private Long getNextId() {
+        log.trace("Getting next id for object film");
         Long maxId = films.keySet().stream()
                 .mapToLong(id -> id)
                 .max()
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,18 +1,19 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -20,25 +21,33 @@ public class UserController {
 
     @GetMapping
     public Collection<User> getAll() {
+        log.info("Start handling request with GET method for /users");
+
         return users.values();
     }
 
     @PostMapping
     public User add(@Valid @RequestBody User user) {
+        log.info("Start handling request with POST method for /users");
+
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
         user.setId(getNextId());
+        log.trace("Put film into hashMap users");
         users.put(user.getId(), user);
         return user;
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User newUser) {
+        log.info("Start handling request with PUT method for /users");
+
         if (newUser.getId() == null) {
             throw new ConditionsNotMetException("Id должен быть указан", "id");
         }
         if (users.containsKey(newUser.getId())) {
+            log.trace("Updating old user object's fields");
             User oldUser = users.get(newUser.getId());
             oldUser.setEmail(newUser.getEmail());
             oldUser.setLogin(newUser.getLogin());
@@ -67,20 +76,24 @@ public class UserController {
                 String errorMessage = error.getDefaultMessage();
                 errors.put(fieldName, errorMessage);
             });
+            log.warn("MethodArgumentNotValidException on handle /users");
         }
         if (ex.getClass().equals(ConditionsNotMetException.class)) {
             ConditionsNotMetException e = (ConditionsNotMetException) ex;
             errors.put(e.getFieldName(), e.getMessage());
+            log.warn("ConditionsNotMetException on handle /users");
         }
         if (ex.getClass().equals(NotFoundException.class)) {
             NotFoundException e = (NotFoundException) ex;
             errors.put(e.getFieldName(), e.getMessage());
+            log.warn("NotFoundException on handle /users");
         }
 
         return errors;
     }
 
     private Long getNextId() {
+        log.trace("Getting next id for object user");
         Long maxId = users.keySet().stream()
                 .mapToLong(id -> id)
                 .max()
